14a4e529a889aba0264eeb3966fb02f32b4ede90
Henrizzle <hltissink@gmail.com>
2026-05-22 12:06:59 +0300

    Extend routing locales to en, af, de, fr, da

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,7 +1,7 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ["en", "af"],
+  locales: ["en", "af", "de", "fr", "da"],
   defaultLocale: "en",
   localePrefix: "as-needed",
 });
